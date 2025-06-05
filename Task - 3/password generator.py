8fcd7e117364162f18200f97080d388409ce1cb9
Krishna bhardwaj <125959501+krixstarr@users.noreply.github.com>
2025-06-06 01:30:19 +0530

    Add files via upload

---
+++ Task - 3/password generator.py
@@ -0,0 +1,40 @@
+import random
+import string
+
+def gen_password(length):
+    characters = (
+        string.digits + string.punctuation + string.hexdigits + string.ascii_letters
+    )
+    password = "".join(random.choice(characters) for _ in range(length))
+    return password
+
+
+# For 1st 2nd 3rd .. Formatting
+num_suffix = (
+    lambda n: "th"
+    if 10 <= n % 100 <= 20
+    else {1: "st", 2: "nd", 3: "rd"}.get(n % 10, "th")
+)
+
+
+def main():
+    try:
+        password_length = int(input("Enter the desired length of the password: "))
+        num_passwords = int(input("Enter the number of passwords to generate: "))
+
+        # Check if the length is valid
+        if password_length <= 0:
+            print("Please enter a valid password length.")
+            return
+
+        # Generate the passwords
+        for _ in range(num_passwords):
+            password = gen_password(password_length)
+            print(f"{_ + 1}{num_suffix(_ + 1)}", "Password: ", password)
+
+    except ValueError:
+        print("Please enter a valid integer for the password length.")
+
+
+if __name__ == "__main__":
+    main()
